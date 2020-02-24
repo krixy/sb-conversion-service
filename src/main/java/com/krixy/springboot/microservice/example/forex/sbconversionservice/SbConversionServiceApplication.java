86141ae11b1270e28be72898ff1f30b239b099a4
krixy <kszajko@gmail.com>
2020-02-24 23:40:05 +0100

    initial commit

---
+++ src/main/java/com/krixy/springboot/microservice/example/forex/sbconversionservice/SbConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.krixy.springboot.microservice.example.forex.sbconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbConversionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krixy/springboot/microservice/example/forex/sbconversionservice/SbConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krixy.springboot.microservice.example.forex.sbconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
